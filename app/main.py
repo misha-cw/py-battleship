5d693fc710bb9038b7d0209cf18b0a19564f18c3
misha-cw <bigun.mykhaylo@gmail.com>
2025-02-26 22:20:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,118 @@
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
+        self.coordinate = (row, column)
+        self.is_alive = is_alive
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(
+            self,
+            start: tuple,
+            end: tuple,
+            is_drowned: bool = False
+    ) -> None:
+        self.decks = [Deck(*deck) for deck in self.get_all_decks(start, end)]
+        self.is_drowned = is_drowned
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def get_deck(self, row: int, column: int) -> Deck | None:
+        for deck in self.decks:
+            if deck.coordinate == (row, column):
+                return deck
+        return None
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def fire(self, row: int, column: int) -> None:
+        coordinate = self.get_deck(row, column)
+        coordinate.is_alive = False
+
+    @staticmethod
+    def get_all_decks(start: tuple[int], end: tuple[int]) -> list:
+        x1, y1 = start
+        x2, y2 = end
+
+        if x1 == x2:
+            return [(x1, y) for y in range(min(y1, y2) , max(y1, y2) + 1)]
+
+        if y1 == y2:
+            return [(x, y1) for x in range(min(x1, x2), max(x1, x2) + 1)]
+
+    def is_alive_deck(self, row: int, column: int) -> bool:
+        return self.get_deck(row, column).is_alive
 
 
 class Battleship:
-    def __init__(self, ships):
+    def __init__(self, ships: list) -> None:
         # Create a dict `self.field`.
         # Its keys are tuples - the coordinates of the non-empty cells,
         # A value for each cell is a reference to the ship
         # which is located in it
-        pass
+        ships = [Ship(*ship) for ship in ships]
+        self.field = {}
+        for ship in ships:
+            for deck in ship.decks:
+                self.field[deck.coordinate] = ship
 
-    def fire(self, location: tuple):
+    def fire(self, location: tuple) -> str:
         # This function should check whether the location
         # is a key in the `self.field`
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
-        pass
+        if location in self.field:
+            ship = self.field[location]
+            ship.fire(*location)
+            if all([not deck.is_alive for deck in ship.decks]):
+                ship.is_drowned = True
+                return "Sunk!"
+            return "Hit!"
+        return "Miss!"
+
+    def print_field(self) -> None:
+        field = [
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+        ]
+
+        for deck in self.field:
+            if not self.field[deck].is_drowned:
+                if self.field[deck].is_alive_deck(*deck):
+                    field[deck[0]][deck[1]] = u"\u25A1"
+                else:
+                    field[deck[0]][deck[1]] = "*"
+            else:
+                field[deck[0]][deck[1]] = "x"
+
+        for i in range(10):
+            print(field[i], "\n")
+
+
+if __name__ == "__main__":
+    battle_ship = Battleship(
+        ships=[
+            ((0, 0), (0, 3)),
+            ((0, 5), (0, 6)),
+            ((0, 8), (0, 9)),
+            ((2, 0), (4, 0)),
+            ((2, 4), (2, 6)),
+            ((2, 8), (2, 9)),
+            ((9, 9), (9, 9)),
+            ((7, 7), (7, 7)),
+            ((7, 9), (7, 9)),
+            ((9, 7), (9, 7)),
+        ]
+    )
+
+    print(
+        battle_ship.fire((0, 4)),  # Miss!
+        battle_ship.fire((0, 3)),  # Hit!
+        battle_ship.fire((0, 2)),  # Hit!
+        battle_ship.fire((0, 1)),  # Hit!
+        battle_ship.fire((0, 0)),  # Sunk!
+    )
+    battle_ship.print_field()
